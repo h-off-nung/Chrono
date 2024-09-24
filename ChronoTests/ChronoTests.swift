e40bf845bea7e4027c8e03b70733bb0d5e9070cd
h-off-nung <h-off-nung@outlook.com>
2024-09-24 13:23:43 +0200

    Initial Commit

---
+++ ChronoTests/ChronoTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChronoTests.swift
+//  ChronoTests
+//
+//  Created by Kirill Bobrov on 24.09.24.
+//
+
+import Testing
+@testable import Chrono
+
+struct ChronoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
